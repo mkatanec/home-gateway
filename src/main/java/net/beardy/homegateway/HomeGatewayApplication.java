64fbfa0f15d08ff7b30fd016e3ec23b7b80ee4f5
mkatanec <matija.katanec@gmail.com>
2022-01-31 18:12:41 +0100

    Initial commit

---
+++ src/main/java/net/beardy/homegateway/HomeGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.beardy.homegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeGatewayApplication.class, args);
+    }
+
+}
