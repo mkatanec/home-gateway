16c8372aeecd544f8cf1dd31d44400cb549b9ff9
mkatanec <matija.katanec@gmail.com>
2024-10-23 18:28:06 +0200

    Add auth to gateway

---
+++ src/main/java/dev/beardy/homegateway/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package dev.beardy.homegateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+//    @Bean
+//    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+//        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+//            .oauth2Login(withDefaults())
+//            .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+//        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+//        return http.build();
+//    }
+
+}
